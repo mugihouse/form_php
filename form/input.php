67001707b2d8c5a9ad1be96570927613f9a2dd2b
mugihouse <mugi.stnk100122@gmail.com>
2024-10-08 10:17:41 +0900

    Add: フォームの追加

---
+++ form/input.php
@@ -0,0 +1,24 @@
+<?php
+  if(!empty($_POST)) {
+    echo  '<pre>';
+    var_dump($_POST);
+    echo '</pre>';
+  }
+?>
+
+<!DOCTYPE html>
+<meta charset="UTF-8">
+<head></head>
+
+<body>
+  <form method="POST" action="input.php">
+  氏名
+  <input type="text" name="your_name">
+  <br>
+  <input type="checkbox" name="sports[]" value="野球">野球
+  <input type="checkbox" name="sports[]" value="サッカー">サッカー
+  <input type="checkbox" name="sports[]" value="バスケ">バスケ
+
+  <input type="submit" value="送信">
+  </form>
+</body>
