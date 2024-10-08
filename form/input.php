5900b3b5c7ecc1bcc780fc1a47510a32c1105972
mugihouse <mugi.stnk100122@gmail.com>
2024-10-08 11:08:18 +0900

    Add: フォームの入力〜完了画面作成

--- form/input.php
+++ form/input.php
@@ -4,6 +4,16 @@
     var_dump($_POST);
     echo '</pre>';
   }
+
+  $pageFlag = 0;
+
+  if(!empty($_POST['btn_confirm'])) {
+    $pageFlag = 1;
+  }
+
+  if(!empty($_POST['btn_submit'])) {
+    $pageFlag = 2;
+  }
 ?>
 
 <!DOCTYPE html>
@@ -11,14 +21,35 @@
 <head></head>
 
 <body>
-  <form method="POST" action="input.php">
-  氏名
-  <input type="text" name="your_name">
-  <br>
-  <input type="checkbox" name="sports[]" value="野球">野球
-  <input type="checkbox" name="sports[]" value="サッカー">サッカー
-  <input type="checkbox" name="sports[]" value="バスケ">バスケ
+  <?php if($pageFlag === 0): ?>
+    <form method="POST" action="input.php">
+      氏名
+      <input type="text" name="your_name">
+      <br>
+      メールアドレス
+      <input type="email" name="email">
+      <br>
+      <input type="submit" name="btn_confirm" value="確認する">
+    </form>
+  <?php endif; ?>
 
-  <input type="submit" value="送信">
-  </form>
+  <?php if($pageFlag === 1): ?>
+    <form method="POST" action="input.php">
+      氏名
+      <?php echo $_POST['your_name']; ?>
+      <br>
+      メールアドレス
+      <?php echo $_POST['email']; ?>
+      <br>
+      <input type="submit" name="btn_submit" value="送信する">
+      <input type="hidden" name="your_name" value="<?php echo $_POST['your_name']; ?>">
+      <input type="hidden" name="email" value="<?php echo $_POST['email']; ?>">
+    </form>
+  <?php endif; ?>
+
+  <?php if($pageFlag === 2): ?>
+    送信が完了しました
+  <?php endif; ?>
+
+
 </body>
