83d19678d1609242ffcad2b3f72b4cee404ef261
mugihouse <mugi.stnk100122@gmail.com>
2024-10-18 08:29:10 +0900

    Add: XSS対策のためサニタイズ

--- form/input.php
+++ form/input.php
@@ -1,8 +1,12 @@
 <?php
-  if(!empty($_POST)) {
-    echo  '<pre>';
-    var_dump($_POST);
-    echo '</pre>';
+  // if(!empty($_POST)) {
+  //   echo  '<pre>';
+  //   var_dump($_POST);
+  //   echo '</pre>';
+  // }
+
+  function h($string) {
+    return htmlspecialchars($string, ENT_QUOTES, 'UTF-8');
   }
 
   $pageFlag = 0;
@@ -24,10 +28,10 @@
   <?php if($pageFlag === 0): ?>
     <form method="POST" action="input.php">
       氏名
-      <input type="text" name="your_name" value="<?php if(!empty($_POST['your_name'])) {echo $_POST['your_name'];} ?>">
+      <input type="text" name="your_name" value="<?php if(!empty($_POST['your_name'])) {echo h($_POST['your_name']);} ?>">
       <br>
       メールアドレス
-      <input type="email" name="email" value="<?php if(!empty($_POST['email'])) {echo $_POST['email'];} ?>">
+      <input type="email" name="email" value="<?php if(!empty($_POST['email'])) {echo h($_POST['email']);} ?>">
       <br>
       <input type="submit" name="btn_confirm" value="確認する">
     </form>
@@ -36,15 +40,15 @@
   <?php if($pageFlag === 1): ?>
     <form method="POST" action="input.php">
       氏名
-      <?php echo $_POST['your_name']; ?>
+      <?php echo h($_POST['your_name']); ?>
       <br>
       メールアドレス
-      <?php echo $_POST['email']; ?>
+      <?php echo h($_POST['email']); ?>
       <br>
       <input type="submit" name="back" value="戻る">
       <input type="submit" name="btn_submit" value="送信する">
-      <input type="hidden" name="your_name" value="<?php echo $_POST['your_name']; ?>">
-      <input type="hidden" name="email" value="<?php echo $_POST['email']; ?>">
+      <input type="hidden" name="your_name" value="<?php echo h($_POST['your_name']); ?>">
+      <input type="hidden" name="email" value="<?php echo h($_POST['email']); ?>">
     </form>
   <?php endif; ?>
 
